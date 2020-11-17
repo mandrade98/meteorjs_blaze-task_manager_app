5d9314ba25cc974ffc28002ba3c99e9269b46761
Murilo Andrade <mandrade.henrique98@outlook.com>
2020-11-17 10:52:16 -0300

    feat: task manager app

---
+++ task-manager-app/tests/main.js
@@ -0,0 +1,21 @@
+import assert from "assert";
+import '../imports/api/tasks.tests';
+
+describe("task-manager-app", function () {
+  it("package.json has correct name", async function () {
+    const { name } = await import("../package.json");
+    assert.strictEqual(name, "task-manager-app");
+  });
+
+  if (Meteor.isClient) {
+    it("client is not server", function () {
+      assert.strictEqual(Meteor.isServer, false);
+    });
+  }
+
+  if (Meteor.isServer) {
+    it("server is not client", function () {
+      assert.strictEqual(Meteor.isClient, false);
+    });
+  }
+});
